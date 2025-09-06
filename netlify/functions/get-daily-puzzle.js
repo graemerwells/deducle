3392547b4b0a8845ef0f88dd18ab1baf7c68f39a
graemerwells <67695322+graemerwells@users.noreply.github.com>
2025-09-06 17:06:25 +0200

    get-daily-puzzle.js

--- netlify/functions/get-daily-puzzle.js
+++ netlify/functions/get-daily-puzzle.js
@@ -1,12 +1,20 @@
 // In file: get-daily-puzzle.js
 
-const { getTodaysPuzzle } = require('./puzzle-logic.js');
+const { PUZZLES_JSON } = require('./puzzles.js');
 
 exports.handler = async function(event, context) {
-  const dailyPuzzle = getTodaysPuzzle();
+    // Daily Puzzle Logic
+    const epochDate = new Date('2024-01-01');
+    const now = new Date();
+    const msSinceEpoch = now - epochDate;
+    const daysSinceEpoch = Math.floor(msSinceEpoch / (1000 * 60 * 60 * 24));
+    const puzzleIndex = daysSinceEpoch % PUZZLES_JSON.length;
+    
+    const dailyPuzzle = PUZZLES_JSON[puzzleIndex];
 
-  return {
-      statusCode: 200,
-      body: JSON.stringify(dailyPuzzle)
-  };
+    // Send the single daily puzzle back to the game.
+    return {
+        statusCode: 200,
+        body: JSON.stringify(dailyPuzzle)
+    };
 };
